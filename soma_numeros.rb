7272cece021b5a5fb380f60db6c45e5262a7a386
Mario R. P. Nascimento <66037130+marionrpn@users.noreply.github.com>
2024-11-05 21:06:58 -0300

    Adiciona script de soma de dois números em Ruby

---
+++ soma_numeros.rb
@@ -0,0 +1,13 @@
+# Solicita que o usuário digite o primeiro número
+print "Digite o primeiro número: "
+numero1 = gets.chomp.to_f
+
+# Solicita que o usuário digite o segundo número
+print "Digite o segundo número: "
+numero2 = gets.chomp.to_f
+
+# Realiza a soma dos dois números
+soma = numero1 + numero2
+
+# Exibe o resultado da soma
+puts "A soma dos dois números é: #{soma}"
